d77dfdf0ac108e326cf4db83bafcc1eb982bbacf
LeandroPata <lmfpata@gmail.com>
2024-10-22 20:31:05 +0100

    Initial Commit

---
+++ app/(auth)/home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import auth from '@react-native-firebase/auth';
+
+const Page = () => {
+    const user = auth().currentUser;
+    return (
+        <View>
+            <Text>Welcome back {user?.email}</Text>
+            <Button title='Sign Out' onPress={() => auth().signOut()} />
+        </View>
+    )
+}
+
+export default Page

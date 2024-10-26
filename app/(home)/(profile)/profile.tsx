4932e7ff8e777bd6252027b1556ec57cdf259b9e
LeandroPata <lmfpata@gmail.com>
2024-10-26 21:51:04 +0100

    Added functionality to select profile picture from gallery or to take photo, updating a member's information and updating it on Firebase, as well as some UI changes

--- app/(home)/(profile)/profile.tsx
+++ app/(home)/(profile)/profile.tsx
@@ -1,68 +1,251 @@
-import { View, Text, StyleSheet, Image, ActivityIndicator, Button } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { router, useLocalSearchParams } from 'expo-router'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ActivityIndicator,
+  Button,
+  TextInput,
+  KeyboardAvoidingView,
+  Pressable,
+  Modal,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useLocalSearchParams } from 'expo-router';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FirebaseError } from 'firebase/app';
 import firestore from '@react-native-firebase/firestore';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function Profile() {
-    const { profileID } = useLocalSearchParams();
-    const [profile, setProfile] = useState();
-    const [loading, setLoading] = useState(false);
-    
-    useEffect(() => {
-        setLoading(true);
-        const subscriber = firestore()
+  const { profileID } = useLocalSearchParams();
+  const insets = useSafeAreaInsets();
+
+  const [profile, setProfile] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [pictureModal, setPictureModal] = useState(false);
+
+  const [name, setName] = useState('');
+  const [memberNumber, setMemberNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhone] = useState('');
+  const [profilePicture, setProfilePicture] = useState<string | null>(null);
+
+  useEffect(() => {
+    setLoading(true);
+    const subscriber = firestore()
+      .collection('users')
+      .doc(profileID)
+      .onSnapshot((documentSnapshot) => {
+        setProfile(documentSnapshot.data());
+        setName(documentSnapshot.data().name);
+        setMemberNumber(documentSnapshot.data().memberNumber);
+        setEmail(documentSnapshot.data().email);
+        setPhone(documentSnapshot.data().phoneNumber);
+        setProfilePicture(documentSnapshot.data().profilePicture);
+      });
+    setLoading(false);
+    return () => subscriber();
+  }, []);
+
+  const pickImage = async () => {
+    setPictureModal(false);
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsMultipleSelection: false,
+      allowsEditing: true,
+      quality: 0.5,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setProfilePicture(result.assets[0].uri);
+    }
+
+    if (result.assets[0].uri != profile.profilePicture) {
+      // Upload picture to Firebase
+    }
+
+    if (
+      profile.profilePicture !=
+      ''
+    ) {
+      // Delete previous picture if it is different from the placeholder
+    }
+  };
+
+  const takePicture = async () => {
+    setPictureModal(false);
+    // Ask the user for the permission to access the camera
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera denied!');
+      return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync();
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setProfilePicture(result.assets[0].uri);
+    }
+  };
+
+  const saveMember = async () => {
+    setLoading(true);
+    try {
+      firestore()
         .collection('users')
         .doc(profileID)
-        .onSnapshot(documentSnapshot => {
-            setProfile(documentSnapshot.data());
+        .update({
+          name: name,
+          memberNumber: memberNumber,
+          email: email,
+          phoneNumber: phoneNumber,
+          profilePicture: profilePicture,
+        })
+        .then(() => {
+          console.log('Member Updated');
         });
-        setLoading(false);
-        return () => subscriber();
-    }, []);
-    
-    return (
+    } catch (e: any) {
+      const err = e as FirebaseError;
+      alert('Adding member failed: ' + err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      <Modal
+        animationType='fade'
+        transparent={true}
+        visible={pictureModal}
+        onRequestClose={() => {
+          setPictureModal(false);
+        }}
+      >
         <View style={styles.container}>
-        {loading || !profile ? (
-            < ActivityIndicator size={'large'} style = {{ margin: 28 }}/>
-        ) : (
-            <>
-            <Image style={styles.picture} src={profile.profilePicture} />
-            <Text style={styles.title}>Name: {profile.name}</Text>
-            <Text style={styles.title}>Member Number: {profile.memberNumber}</Text>
-            <Text style={styles.title}>Email: {profile.email}</Text>
-            <Text style={styles.title}>Phone Number: {profile.phoneNumber}</Text>
-            <Text style={styles.title}>Added Date: {new Date(profile.addedDate.toDate()).toLocaleDateString('pt-pt')}</Text>
-            <Button onPress={() => {
-                router.push({ pathname: '/(home)/(profile)/editMember', params: { profileID: profileID } });
-            }}
-            title="Edit Member" />
-            </>
-        )}
+          <View style={styles.modalContainer}>
+            <Button
+              title='Pick an image from camera roll'
+              onPress={pickImage}
+            />
+            <Button title='Take Picture' onPress={takePicture} />
+          </View>
         </View>
-    )
+      </Modal>
+      {loading || !profile ? (
+        <ActivityIndicator size={'large'} style={{ margin: 28 }} />
+      ) : editing ? (
+        <KeyboardAvoidingView behavior='padding'>
+          <Pressable
+            onPress={() => {
+              setPictureModal(true);
+            }}
+          >
+            <Image style={styles.picture} src={profilePicture} />
+          </Pressable>
+          <TextInput
+            style={styles.input}
+            value={name}
+            onChangeText={setName}
+            autoCapitalize='words'
+            keyboardType='default'
+            placeholder='Name'
+          />
+          <TextInput
+            style={styles.input}
+            value={memberNumber.toString()}
+            onChangeText={setMemberNumber}
+            autoCapitalize='none'
+            keyboardType='numeric'
+            placeholder='Member Number (leave empty for automatic assignment)'
+          />
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize='none'
+            keyboardType='email-address'
+            placeholder='Email'
+          />
+          <TextInput
+            style={styles.input}
+            value={phoneNumber}
+            onChangeText={setPhone}
+            autoCapitalize='none'
+            inputMode='tel'
+            keyboardType='phone-pad'
+            placeholder='Phone Number'
+          />
+          <Button onPress={saveMember} title='Save' />
+        </KeyboardAvoidingView>
+      ) : (
+        <View>
+          <Image style={styles.picture} src={profilePicture} />
+          <Text style={styles.title}>Name: {name}</Text>
+          <Text style={styles.title}>Member Number: {memberNumber}</Text>
+          <Text style={styles.title}>Email: {email}</Text>
+          <Text style={styles.title}>Phone Number: {phoneNumber}</Text>
+          <Text style={styles.title}>
+            Added Date:{' '}
+            {new Date(profile.addedDate.toDate()).toLocaleDateString('pt-pt')}
+          </Text>
+          <Button
+            onPress={() => {
+              setEditing(true);
+            }}
+            title='Edit Member'
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginHorizontal: 20,
-        flex: 1,
-        justifyContent: 'center'
+  container: {
+    //marginHorizontal: 20,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    input: {
-        marginVertical: 4,
-        height: 50,
-        borderWidth: 1,
-        borderRadius: 1,
-        padding: 5,
-        backgroundColor: '#ffffff'
-    },
-    picture: {
-        width: 100,
-        height: 100,
-        resizeMode: 'contain',
-        alignSelf: 'center'
-    },
-    title: {
-        fontSize: 15,
-    }
-})
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  input: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 1,
+    padding: 5,
+    backgroundColor: '#ffffff',
+  },
+  picture: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: 15,
+  },
+});

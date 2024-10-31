4f45db8e02d119ab55e8a1db3edf5b511e090333
LeandroPata <lmfpata@gmail.com>
2024-10-31 22:49:15 +0000

    Added checks for the inputs and added a modal to confirm password

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,49 @@
 import { useState } from 'react';
 import {
   View,
+  Text,
   StyleSheet,
   KeyboardAvoidingView,
   TextInput,
   Button,
   ActivityIndicator,
+  Modal,
 } from 'react-native';
 import { FirebaseError } from 'firebase/app';
 import auth from '@react-native-firebase/auth';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function Index() {
+  const [loading, setLoading] = useState(false);
+  const [confirmModal, setConfirmModal] = useState(false);
+  const insets = useSafeAreaInsets();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const signUp = async () => {
     setLoading(true);
+    setConfirmModal(true);
+
+    if (!email.trim() && !password.trim()) {
+      alert('Email and password fields are empty!');
+      setLoading(false);
+      return;
+    } else if (!email.trim()) {
+      alert('Email field is empty!');
+      setLoading(false);
+      return;
+    } else if (!password.trim()) {
+      alert('Password field is empty!');
+      setLoading(false);
+      return;
+    } else if (!email.includes('@') || !email.includes('.')) {
+      alert('Email is in the wrong format!');
+      setLoading(false);
+      return;
+    }
+
     try {
       await auth().createUserWithEmailAndPassword(email, password);
     } catch (e: any) {
@@ -30,6 +57,24 @@ export default function Index() {
 
   const signIn = async () => {
     setLoading(true);
+    if (!email.trim() && !password.trim()) {
+      alert('Email and password fields are empty!');
+      setLoading(false);
+      return;
+    } else if (!email.trim()) {
+      alert('Email field is empty!');
+      setLoading(false);
+      return;
+    } else if (!password.trim()) {
+      alert('Password field is empty!');
+      setLoading(false);
+      return;
+    } else if (!email.includes('@') || !email.includes('.')) {
+      alert('Email is in the wrong format!');
+      setLoading(false);
+      return;
+    }
+
     try {
       await auth().signInWithEmailAndPassword(email, password);
     } catch (e: any) {
@@ -42,8 +87,32 @@ export default function Index() {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { paddingTop: insets.top }]}>
       <KeyboardAvoidingView behavior='padding'>
+        <Modal
+          animationType='fade'
+          transparent={true}
+          visible={confirmModal}
+          onRequestClose={() => {
+            setConfirmModal(false);
+          }}
+        >
+          <View style={styles.modalContainer}>
+            <TextInput
+              style={styles.input}
+              value={confirmPassword}
+              onChangeText={setConfirmPassword}
+              secureTextEntry
+              placeholder='Confirm Password'
+            />
+            <Button
+              title='Confirm'
+              onPress={() => {
+                setConfirmModal(false);
+              }}
+            />
+          </View>
+        </Modal>
         <TextInput
           style={styles.input}
           value={email}
@@ -78,11 +147,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
+  modalContainer: {
+    flex: 1,
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    //alignSelf: 'center',
+    width: '75%',
+    height: '50%',
+  },
   input: {
     marginVertical: 4,
     height: 50,
     borderWidth: 1,
-    borderRadius: 1,
+    borderRadius: 10,
     padding: 10,
     backgroundColor: '#ffffff',
   },

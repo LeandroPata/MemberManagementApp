f5047afeaaf704be79720aca6ad58320773386a8
LeandroPata <lmfpata@gmail.com>
2024-11-03 21:52:49 +0000

    Converted components to their equivalent from react-native-paper in  addMember

--- app/(home)/addMember.tsx
+++ app/(home)/addMember.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
+import { View, StyleSheet, KeyboardAvoidingView, Text } from 'react-native';
 import {
-  View,
-  Text,
-  StyleSheet,
-  KeyboardAvoidingView,
-  TextInput,
   Button,
-  ActivityIndicator,
-} from 'react-native';
+  TextInput,
+  Switch,
+  PaperProvider,
+  Avatar,
+} from 'react-native-paper';
 import DatePicker from 'react-native-date-picker';
 import { FirebaseError } from 'firebase/app';
 import firestore, { Timestamp } from '@react-native-firebase/firestore';
@@ -17,6 +16,7 @@ export default function AddMember() {
   const insets = useSafeAreaInsets();
   const [loading, setLoading] = useState(false);
   const [dateModal, setDateModal] = useState(false);
+  const [autoNumber, setAutoNumber] = useState(true);
 
   const [name, setName] = useState('');
   const [memberNumber, setMemberNumber] = useState('');
@@ -53,8 +53,12 @@ export default function AddMember() {
       return;
     }
 
-    if (!memberNumber.trim()) {
+    if (autoNumber) {
       await assignMemberNumber();
+    } else if (!memberNumber.trim()) {
+      alert('Member number is mandatory!');
+      setLoading(false);
+      return;
     } else {
       minNumber = parseInt(memberNumber);
     }
@@ -91,69 +95,91 @@ export default function AddMember() {
   };
 
   return (
-    <View style={[styles.container, { paddingTop: insets.top }]}>
-      <KeyboardAvoidingView behavior='padding'>
-        <TextInput
-          style={[
-            styles.input,
-            { borderColor: !name.trim() ? '#ee1311' : '#000000' },
-          ]}
-          value={name}
-          onChangeText={setName}
-          autoCapitalize='words'
-          keyboardType='default'
-          placeholder='Name'
-        />
-        <TextInput
-          style={styles.input}
-          value={memberNumber}
-          onChangeText={setMemberNumber}
-          autoCapitalize='none'
-          keyboardType='numeric'
-          placeholder='Member Number (leave empty for automatic assignment)'
-        />
-        <TextInput
-          style={styles.input}
-          value={email}
-          onChangeText={setEmail}
-          autoCapitalize='none'
-          keyboardType='email-address'
-          placeholder='Email'
-        />
-        <TextInput
-          style={styles.input}
-          value={phoneNumber}
-          onChangeText={setPhone}
-          autoCapitalize='none'
-          inputMode='tel'
-          keyboardType='phone-pad'
-          placeholder='Phone Number'
-        />
-        <Text>{endDate.toLocaleDateString('pt-pt')}</Text>
-        <Button title='Set End Date' onPress={() => setDateModal(true)} />
-        <DatePicker
-          modal
-          mode='date'
-          locale='pt-pt'
-          open={dateModal}
-          date={endDate}
-          onConfirm={(endDate) => {
-            setDateModal(false);
-            setEndDate(endDate);
-          }}
-          onCancel={() => {
-            setDateModal(false);
-          }}
-        />
-        {loading ? (
-          <ActivityIndicator size={'small'} style={{ margin: 28 }} />
-        ) : (
-          <>
-            <Button onPress={addMember} title='Add Member' />
-          </>
-        )}
-      </KeyboardAvoidingView>
-    </View>
+    <PaperProvider>
+      <View style={[styles.container, { paddingTop: insets.top }]}>
+        <KeyboardAvoidingView behavior='padding'>
+          <TextInput
+            style={styles.input}
+            value={name}
+            onChangeText={setName}
+            autoCapitalize='words'
+            keyboardType='default'
+            label='Name'
+          />
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize='none'
+            keyboardType='email-address'
+            label='Email'
+          />
+          <TextInput
+            style={styles.input}
+            value={phoneNumber}
+            onChangeText={setPhone}
+            autoCapitalize='none'
+            inputMode='tel'
+            keyboardType='phone-pad'
+            label='Phone Number'
+          />
+          <View
+            style={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent: 'space-between',
+            }}
+          >
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flex: 2,
+              }}
+            >
+              <Text>Auto Number</Text>
+              <Switch value={autoNumber} onValueChange={setAutoNumber} />
+            </View>
+            <TextInput
+              disabled={autoNumber}
+              style={[styles.input, { flex: 3 }]}
+              value={memberNumber}
+              onChangeText={setMemberNumber}
+              autoCapitalize='none'
+              keyboardType='numeric'
+              label='Member Number'
+              placeholder='(leave empty for automatic assignment)'
+            />
+          </View>
+          <Button onPress={() => setDateModal(true)}>
+            End Date: {endDate.toLocaleDateString('pt-pt')}
+          </Button>
+          <DatePicker
+            modal
+            mode='date'
+            locale='pt-pt'
+            open={dateModal}
+            date={endDate}
+            onConfirm={(endDate) => {
+              setDateModal(false);
+              setEndDate(endDate);
+            }}
+            onCancel={() => {
+              setDateModal(false);
+            }}
+          />
+          <Button
+            style={styles.button}
+            mode='elevated'
+            loading={loading}
+            onPress={addMember}
+          >
+            Add Member
+          </Button>
+        </KeyboardAvoidingView>
+      </View>
+    </PaperProvider>
   );
 }
 
@@ -164,11 +190,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   input: {
-    marginVertical: 4,
-    height: 50,
-    borderWidth: 1,
-    borderRadius: 1,
-    padding: 5,
-    backgroundColor: '#ffffff',
+    marginVertical: 2,
+  },
+  button: {
+    marginVertical: 3,
   },
 });

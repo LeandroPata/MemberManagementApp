8848287f6e315a483dfbcf12735710100180593d
LeandroPata <lmfpata@gmail.com>
2024-11-12 14:41:42 +0000

    Implemented Drawer component

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,20 +1,63 @@
 import React from 'react';
+import { TouchableOpacity, View } from 'react-native';
 import { useTheme } from 'react-native-paper';
-import { Stack } from 'expo-router';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from '@/components/gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 const HomeLayout = () => {
   const theme = useTheme();
+  const insets = useSafeAreaInsets();
+
   return (
-    <Stack
+    <Drawer
       screenOptions={{
-        contentStyle: { backgroundColor: theme.colors.background },
+        sceneContainerStyle: {
+          backgroundColor: theme.colors.background,
+        },
+        drawerStyle: { backgroundColor: theme.colors.background },
+        drawerLabelStyle: { color: theme.colors.onBackground },
+        drawerActiveTintColor: theme.colors.primary,
+        headerStyle: {
+          backgroundColor: theme.colors.background,
+        },
+        headerTitleStyle: { color: theme.colors.onBackground },
+        headerShadowVisible: false,
+        header: ({ navigation }) => (
+          <View style={{ paddingTop: insets.top, paddingHorizontal: 5 }}>
+            <TouchableOpacity
+              onPress={() => navigation.toggleDrawer()}
+              style={{ maxWidth: 36 }}
+            >
+              <Ionicons
+                name='menu'
+                size={32}
+                color={theme.colors.onBackground}
+                style={{ alignSelf: 'center' }}
+              />
+            </TouchableOpacity>
+          </View>
+        ),
       }}
     >
-      <Stack.Screen name='home' options={{ headerShown: false }} />
-      <Stack.Screen name='addMember' options={{ headerShown: false }} />
-      <Stack.Screen name='searchMember' options={{ headerShown: false }} />
-      <Stack.Screen name='(profile)' options={{ headerShown: false }} />
-    </Stack>
+      <Drawer.Screen
+        name='home'
+        options={{ drawerLabel: 'Home', title: 'Home' }}
+      />
+      <Drawer.Screen
+        name='addMember'
+        options={{ drawerLabel: 'Add Member', title: 'Add Member' }}
+      />
+      <Drawer.Screen
+        name='searchMember'
+        options={{ drawerLabel: 'Search Member', title: 'Search Member' }}
+      />
+      <Drawer.Screen
+        name='(profile)'
+        options={{ drawerItemStyle: { display: 'none' } }}
+      />
+    </Drawer>
   );
 };
 

d48a47cd8b7a32d98676467b674bf6481af0b62e
LeandroPata <lmfpata@gmail.com>
2024-12-17 19:21:06 +0000

    Started setting an accurate version

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'MemberManagementApp',
   slug: 'MemberManagementApp',
-  version: '1.0.0',
+  version: '0.10.1',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'myapp',

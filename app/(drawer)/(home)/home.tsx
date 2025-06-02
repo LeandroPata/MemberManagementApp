733c1cdc6efc18946ac1bd88ffbab0fea252d9be
LeandroPata <lmfpata@gmail.com>
2025-06-02 20:03:01 +0100

    Added a custom MenuComponent to implement menus on some buttons or other interactive elements

--- app/(drawer)/(home)/home.tsx
+++ app/(drawer)/(home)/home.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Image } from 'react-native';
 import { Button } from 'react-native-paper';
 import { router } from 'expo-router';
 import { useTranslation } from 'react-i18next';
 import { globalStyles } from '@/styles/global';
+import { goToNFCSettings, readNFC } from '@/utils/NFC';
+import DialogConfirmation from '@/components/DialogConfirmation';
+import { checkDoc } from '@/utils/Firebase';
+import MenuComponent from '@/components/MenuComponent';
+import { goToProfile } from '@/utils/Utils';
 
 /* import firestore, { Timestamp } from '@react-native-firebase/firestore';
 import type { FirebaseError } from 'firebase/app'; */
 
 export default function Home() {
 	const { t } = useTranslation();
+
+	// All the logic to implement DialogConfirmation
+	const [dialogConfirmationVisible, setDialogConfirmationVisible] =
+		useState(false);
+	const onDismissDialogConfirmation = () => setDialogConfirmationVisible(false);
 
 	/* const convertDB = async () => {
 		await firestore()
@@ -33,80 +43,107 @@ export default function Home() {
 			});
 	}; */
 
+	const onConfirmationDialogConfirmation = () => {
+		setDialogConfirmationVisible(false);
+		goToNFCSettings();
+	};
+
+	const rNFC = async () => {
+		const payload = await readNFC();
+		if (!payload) {
+			setDialogConfirmationVisible(true);
+			MenuComponent;
+			return false;
+		}
+
+		const docCheck = await checkDoc(payload);
+		if (docCheck) goToProfile(payload);
+	};
+
 	return (
-		<View
-			style={globalStyles.container.home}
-			testID='HomePage'
-		>
-			<View style={globalStyles.container.image}>
-				<Image
-					style={globalStyles.image.global}
-					source={require('@/assets/images/logoReact.png')}
-					testID='HomeLogo'
-				/>
+		<>
+			<DialogConfirmation
+				text={t('nfc.goToSettings')}
+				visible={dialogConfirmationVisible}
+				onDismiss={onDismissDialogConfirmation}
+				onConfirmation={onConfirmationDialogConfirmation}
+				onDismissText='Cancel'
+				onConfirmationText='Go to Settings'
+			/>
+			<View
+				style={globalStyles.container.home}
+				testID='HomePage'
+			>
+				<View style={globalStyles.container.image}>
+					<Image
+						style={globalStyles.image.global}
+						source={require('@/assets/images/logoReact.png')}
+						testID='HomeLogo'
+					/>
+				</View>
+				<View style={globalStyles.container.button}>
+					<Button
+						style={globalStyles.button.global}
+						contentStyle={globalStyles.buttonContent.global}
+						labelStyle={globalStyles.buttonText.global}
+						icon='account-plus'
+						mode='elevated'
+						//loading={loginLoading}
+						onPress={() => router.push('/(drawer)/(home)/addMember')}
+						testID='AddButton'
+					>
+						{t('home.addMember')}
+					</Button>
+					<Button
+						style={globalStyles.button.global}
+						contentStyle={globalStyles.buttonContent.global}
+						labelStyle={globalStyles.buttonText.global}
+						icon='account-search'
+						mode='elevated'
+						//loading={loginLoading}
+						onPress={() => router.push('/(drawer)/(home)/searchMember')}
+						testID='SearchButton'
+					>
+						{t('home.searchMember')}
+					</Button>
+					<Button
+						style={globalStyles.button.global}
+						contentStyle={globalStyles.buttonContent.global}
+						labelStyle={globalStyles.buttonText.global}
+						icon='nfc'
+						mode='elevated'
+						//loading={loginLoading}
+						onPress={() => router.push('/(drawer)/(home)/updateMembers')}
+						testID='UpdateMembersButton'
+					>
+						{t('home.updateMembers')}
+					</Button>
+					<Button
+						style={globalStyles.button.global}
+						contentStyle={globalStyles.buttonContent.global}
+						labelStyle={globalStyles.buttonText.global}
+						icon='database'
+						mode='elevated'
+						//loading={loginLoading}
+						onPress={() => router.push('/(drawer)/(home)/importExport')}
+						testID='ImportExportButton'
+					>
+						{t('home.importExport')}
+					</Button>
+					<Button
+						style={globalStyles.button.global}
+						contentStyle={globalStyles.buttonContent.global}
+						labelStyle={globalStyles.buttonText.global}
+						icon='nfc'
+						mode='elevated'
+						//loading={loginLoading}
+						onPress={rNFC}
+						testID='NFCButton'
+					>
+						{t('home.nfc')}
+					</Button>
+				</View>
 			</View>
-			<View style={globalStyles.container.button}>
-				<Button
-					style={globalStyles.button.global}
-					contentStyle={globalStyles.buttonContent.global}
-					labelStyle={globalStyles.buttonText.global}
-					icon='account-plus'
-					mode='elevated'
-					//loading={loginLoading}
-					onPress={() => router.push('/(drawer)/(home)/addMember')}
-					testID='AddButton'
-				>
-					{t('home.addMember')}
-				</Button>
-				<Button
-					style={globalStyles.button.global}
-					contentStyle={globalStyles.buttonContent.global}
-					labelStyle={globalStyles.buttonText.global}
-					icon='account-search'
-					mode='elevated'
-					//loading={loginLoading}
-					onPress={() => router.push('/(drawer)/(home)/searchMember')}
-					testID='SearchButton'
-				>
-					{t('home.searchMember')}
-				</Button>
-				<Button
-					style={globalStyles.button.global}
-					contentStyle={globalStyles.buttonContent.global}
-					labelStyle={globalStyles.buttonText.global}
-					icon='nfc'
-					mode='elevated'
-					//loading={loginLoading}
-					onPress={() => router.push('/(drawer)/(home)/updateMembers')}
-					testID='UpdateMembersButton'
-				>
-					{t('home.updateMembers')}
-				</Button>
-				<Button
-					style={globalStyles.button.global}
-					contentStyle={globalStyles.buttonContent.global}
-					labelStyle={globalStyles.buttonText.global}
-					icon='database'
-					mode='elevated'
-					//loading={loginLoading}
-					onPress={() => router.push('/(drawer)/(home)/importExport')}
-					testID='ImportExportButton'
-				>
-					{t('home.importExport')}
-				</Button>
-				<Button
-					style={globalStyles.button.global}
-					contentStyle={globalStyles.buttonContent.global}
-					labelStyle={globalStyles.buttonText.global}
-					icon='nfc'
-					mode='elevated'
-					//loading={loginLoading}
-					onPress={() => router.push('/(drawer)/(home)/nfc')}
-					testID='NFCButton'
-				>
-					{t('home.nfc')}
-				</Button>
-			</View>
-		</View>
+		</>
 	);
 }
---
+++ components/MenuComponent.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import type {
+	Animated,
+	GestureResponderEvent,
+	StyleProp,
+	ViewStyle,
+} from 'react-native';
+import { Menu } from 'react-native-paper';
+
+type MenuComponentProps = {
+	anchor: React.ReactElement;
+	style?: StyleProp<ViewStyle>;
+	contentStyle?: Animated.WithAnimatedValue<StyleProp<ViewStyle>>;
+	children: React.ReactElement | Array<React.ReactElement>;
+};
+
+const MenuComponent = (props: MenuComponentProps) => {
+	const [visible, setVisible] = useState(false);
+	const [menuAnchor, setMenuAnchor] = useState({ x: 0, y: 0 });
+
+	const openMenu = () => setVisible(true);
+	const closeMenu = () => setVisible(false);
+
+	const onIconPress = (event: GestureResponderEvent) => {
+		const { pageX, pageY } = event.nativeEvent;
+		const anchor = {
+			x: pageX,
+			y: pageY,
+		};
+
+		setMenuAnchor(anchor);
+		openMenu();
+	};
+
+	return (
+		<>
+			{React.cloneElement(props.anchor, { onLongPress: onIconPress })}
+			<Menu
+				visible={visible}
+				onDismiss={closeMenu}
+				anchor={menuAnchor}
+				//style={props.style}
+				//style={{ borderWidth: 5, borderRadius: 20 }}
+				contentStyle={props.contentStyle}
+			>
+				{props.children}
+			</Menu>
+		</>
+	);
+};
+
+export default MenuComponent;

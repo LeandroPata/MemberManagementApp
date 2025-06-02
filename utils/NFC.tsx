568e2b4354691a76472a4ed482393ead6f463d67
LeandroPata <lmfpata@gmail.com>
2025-06-02 20:05:42 +0100

    Added all NFC implementation to its own file top use across the whole app when necessary

---
+++ utils/NFC.tsx
@@ -0,0 +1,90 @@
+import NfcManager, {
+	Ndef,
+	NfcTech,
+	type TagEvent,
+} from 'react-native-nfc-manager';
+
+// To decode the payload into plain text
+const decodePayload = (tag: TagEvent) => {
+	const payload = Ndef.text.decodePayload(tag?.ndefMessage[0].payload);
+	console.log(payload);
+	return payload;
+};
+
+// To take the user to the settings to enable NFC if not enabled
+export const goToNFCSettings = () => {
+	//setDialogConfirmationVisible(false);
+	NfcManager.goToNfcSetting();
+};
+
+// Check if NFC is supported and is currently enabled
+export const checkNFC = async () => {
+	const supported = await NfcManager.isSupported();
+	console.log(`NFC Supported: ${supported}`);
+	const enabled = await NfcManager.isEnabled();
+	console.log(`NFC Enabled: ${enabled}`);
+
+	if (supported && enabled) return true;
+	else {
+		return false;
+	}
+};
+
+// Try to read payload from NFC
+export const readNFC = async () => {
+	let payload = '';
+
+	try {
+		const check = await checkNFC();
+		if (!check) {
+			return payload;
+		}
+
+		await NfcManager.requestTechnology(NfcTech.Ndef);
+
+		const tag = await NfcManager.getTag();
+		console.log(tag);
+		if (tag) payload = decodePayload(tag);
+
+		return payload;
+	} catch (e: any) {
+		console.log(`Error: ${e.message}`);
+		//setLoadingRead(false);
+	} finally {
+		NfcManager.cancelTechnologyRequest();
+		//setLoadingRead(false);
+	}
+};
+
+// Im aware that, as is, anyone with an external NFC tool can
+// get the plain text from the card, possibly resulting in their duplication
+// A solution would be to pass the id through a cypher first and then encoding it
+export const writeNFC = async (id: string) => {
+	//setLoadingWrite(true);
+	let result = false;
+
+	try {
+		const check = await checkNFC();
+		if (!check || !id) {
+			//setLoadingWrite(false);
+			return false;
+		}
+
+		await NfcManager.requestTechnology(NfcTech.Ndef);
+
+		const payload = Ndef.encodeMessage([Ndef.textRecord(id)]);
+		console.log(payload);
+
+		if (payload) {
+			await NfcManager.ndefHandler.writeNdefMessage(payload);
+			result = true;
+		}
+	} catch (e: any) {
+		console.log(`Error: ${e.message}`);
+		//setLoadingWrite(false);
+	} finally {
+		NfcManager.cancelTechnologyRequest();
+		//setLoadingWrite(false);
+	}
+	return result;
+};
